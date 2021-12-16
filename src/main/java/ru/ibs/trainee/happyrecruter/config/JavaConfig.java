3e9c054d68650335282b01d3f969c4ba0276066c
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-12-16 20:13:11 +0300

    upd

--- src/main/java/ru/ibs/trainee/happyrecruter/config/JavaConfig.java
+++ src/main/java/ru/ibs/trainee/happyrecruter/config/JavaConfig.java
@@ -24,7 +24,6 @@ public class JavaConfig {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-		driverManagerDataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
 		driverManagerDataSource.setUsername(env.getProperty("spring.datasource.username"));
 		driverManagerDataSource.setPassword(env.getProperty("spring.datasource.password"));
 		driverManagerDataSource.setUrl(env.getProperty("spring.datasource.url"));
